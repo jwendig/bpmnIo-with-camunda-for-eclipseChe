06567b574706612c987d444ed93ee23cce688c20
things4it <things4it@gmail.com>
2015-09-24 14:03:43 +0200

    - namespace fox and element failedJobRetryTimeCycle added

--- app/index.js
+++ app/index.js
@@ -1,25 +1,20 @@
 // BEGIN COPY BLOG (copy this to the top of the generated .js document)
-
 var bpmnIo_fkt_createNewModeler;
-
 // END COPY BLOG
 
-bpmnIo_fkt_createNewModeler = function(wrapperId){
-	
+bpmnIo_fkt_createNewModeler = function(wrapperId) {
+
 	var renderer = new BpmnModeler({
-		container : $('#'+wrapperId),
+		container : $('#' + wrapperId),
 		moddleExtensions : {
-			camunda : camundaMetaModel
+			camunda : camundaMetaModel,
+			fox : foxMetaModel
 		}
 	});
-	
+
 	return renderer;
 };
 
 var $ = require('jquery'), BpmnModeler = require('bpmn-js/lib/Modeler');
 var camundaMetaModel = require('../resources/camunda');
-
-
-
-
-
+var foxMetaModel = require('../resources/fox');
